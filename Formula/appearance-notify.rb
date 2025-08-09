e59b2f80ff2b685d4cfd95cb808587669c221f64
George Zhang <georgezhangtj97@gmail.com>
2025-08-09 02:20:56 -0700

    chore(homebrew): simplify to universal tarball; remove arch conditionals

---
+++ Formula/appearance-notify.rb
@@ -0,0 +1,43 @@
+class AppearanceNotify < Formula
+  desc "macOS daemon that executes hooks on system appearance changes"
+  homepage "https://github.com/odysseus0/appearance-notify"
+  version "0.2.0"
+  license "MIT"
+  
+  depends_on macos: :sonoma
+  
+    end
+  
+  def install
+    bin.install "appearance-notify"
+    
+    (prefix/"io.github.odysseus0.appearance-notify.plist").write <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+          <key>Label</key>
+          <string>io.github.odysseus0.appearance-notify</string>
+          <key>ProgramArguments</key>
+          <array>
+              <string>#{opt_bin}/appearance-notify</string>
+              <string>daemon</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <true/>
+      </dict>
+      </plist>
+    EOS
+  end
+  
+  service do
+    run [opt_bin/"appearance-notify", "daemon"]
+    keep_alive true
+  end
+  
+  test do
+    assert_match version.to_s, shell_output("#{bin}/appearance-notify --version")
+  end
+end
